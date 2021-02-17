c5a74463f7f9cdd8fbc2e5bbaf25da3c168f72f4
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2021-02-17 03:12:03 +0000

    C++ exemplary application added.

--- exemplary_program.cpp
+++ exemplary_program.cpp
@@ -1,5 +1,18 @@
+#include <opencv2/imgcodecs.hpp>
 #include "superlibrary.h"
 
 int main(int argc, char **argv) {
-
+  // Read demo image
+  cv::Mat im = cv::imread("demo_image.png"); 
+  
+  // Use C++ coolFunction()
+  cv::Mat blur = coolFunction(im);
+  
+  // Use C++ CoolClass::toGray()
+  CoolClass cc;
+  cv::Mat gray = cc.toGray(im);
+  
+  // Write output images
+  cv::imwrite("blur.png", blur);
+  cv::imwrite("gray.png", gray);
 }
